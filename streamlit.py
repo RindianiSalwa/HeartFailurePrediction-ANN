9ecc39d702ef0390461a8136711352de5dc61a73
RindianiSalwa <salwarindiani213@gmail.com>
2025-04-28 13:17:49 +0700

    streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -22,9 +22,14 @@ ejection_fraction = st.number_input('Ejection Fraction (%)', min_value=10, max_v
 serum_creatinine = st.number_input('Serum Creatinine (mg/dL)', min_value=0.1, max_value=10.0, value=1.0)
 serum_sodium = st.number_input('Serum Sodium (mEq/L)', min_value=100, max_value=150, value=135)
 time = st.number_input('Follow-up Time (hari)', min_value=1, max_value=400, value=100)
-anaemia = st.selectbox('Anaemia', [0, 1])
-high_blood_pressure = st.selectbox('Tekanan Darah Tinggi', [0, 1])
-sex = st.selectbox('Jenis Kelamin (1=Pria, 0=Wanita)', [0, 1])
+anaemia = st.selectbox('Anaemia', ['Tidak Ada', 'Ada'])
+high_blood_pressure = st.selectbox('Tekanan Darah Tinggi', ['Tidak Ada', 'Ada'])
+sex = st.selectbox('Jenis Kelamin', ['Wanita', 'Pria'])
+
+#mapping
+anaemia = 1 if anaemia == 'Ada' else 0
+high_blood_pressure = 1 if high_blood_pressure == 'Ada' else 0
+sex = 1 if sex == 'Pria' else 0
 
 # Button prediksi
 if st.button('Prediksi'):
